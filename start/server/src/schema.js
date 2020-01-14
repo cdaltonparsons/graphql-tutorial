d8ef2243848ddb92c8901caa660ddf66545103b5
Charlie Parsons <cdaltonparsons@gmail.com>
2020-01-13 17:14:01 -0700

    finished section 3 of tutorial

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,8 +1,48 @@
-const { gql } = require("apollo-server");
+const { gql } = require('apollo-server');
 
 const typeDefs = gql`
-  # Schema will go here
-  # An exclamation point after a declared field's type means "this field's value can never be null"
+  type Query {
+    launches(
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type Mutation {
+    # if false, signup failed -- check errors
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+
+    # if false, cancellation failed -- check errors
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+
+    login(email: String): String # login token
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection {
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
+  }
+
   type Launch {
     id: ID!
     site: String
@@ -16,7 +56,7 @@ const typeDefs = gql`
     name: String
     type: String
   }
-  # If a declared field's type is in [Square Brackets], it's an array of the specified type. If an array has an exclamation point after it, the array cannot be null, but it can be empty.
+
   type User {
     id: ID!
     email: String!
@@ -24,38 +64,13 @@ const typeDefs = gql`
   }
 
   type Mission {
-    name: String
-    missionPatch(size: PatchSize): String
+    # ... with rest of schema
+    missionPatch(mission: String, size: PatchSize): String
   }
 
   enum PatchSize {
     SMALL
     LARGE
-  }
-  # This Query type defines three available queries for clients to execute: launches, launch, and me.
-  # The launches query will return an array of all upcoming Launches.
-  # The launch query will return a single Launch that corresponds to the id argument provided to the query.
-  # The me query will return details for the User that's currently logged in.
-  type Query {
-    launches: [Launch]!
-    launch(id: ID!): Launch
-    me: User
-  }
-
-# This Mutation type defines three available mutations for clients to execute: bookTrips, cancelTrip, and login.
-# The bookTrips mutation enables a logged-in user to book a trip on one or more Launches (specified by an array of launch IDs).
-# The cancelTrip mutation enables a logged-in user to cancel a trip that they previously booked.
-# The login mutation enables a user to log in by providing their email address.
-  type Mutation {
-    bookTrips(launchIds: [ID]!): TripUpdateResponse!
-    cancelTrip(launchId: ID!): TripUpdateResponse!
-    login(email: String): String # login token
-  }
-
-  type TripUpdateResponse {
-      success: Boolean!
-      message: String
-      launches: [Launch]
   }
 `;
 
